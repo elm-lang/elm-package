9d9d30962d1b100de34d3dddde710e9a5e444482
Evan Czaplicki <info@elm-lang.org>
2013-09-29 17:39:36 -0700

    minor clarification in flags

--- Main.hs
+++ Main.hs
@@ -20,13 +20,13 @@ commands =
       &= details [ "Examples:"
                  , "  elm-get install            # install everything needed by build.json"
                  , "  elm-get install Vector2D   # install library from the central repo"
-                 , "  elm-get install sam/Array  # install a specific github repo" ]
+                 , "  elm-get install tom/Array  # install a specific github repo" ]
     , Update { libs_ = [] &= args &= typ "LIBRARY" }
       &= help "Check for updates to any local libraries, ask to upgrade."
       &= details [ "Examples:"
                  , "  elm-get update             # check for updates to local libraries"
                  , "  elm-get update Vector2D    # update library from the central repo"
-                 , "  elm-get update sam/Array   # update a specific github repo" ]
+                 , "  elm-get update tom/Array   # update from a specific github repo" ]
     , Publish { repo = Nothing &= args &= typ "REPO" }
       &= help "Publish project to the central repository."
       &= details []
