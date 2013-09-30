a93c95c01920642b5890bba45faa5cd15b50c1b1
Evan Czaplicki <info@elm-lang.org>
2013-09-29 17:20:24 -0700

    Add examples for each mode, make info a bit prettier

--- Main.hs
+++ Main.hs
@@ -17,25 +17,34 @@ data Commands
 commands =
     [ Install { libs = [] &= args &= typ "LIBRARY" }
       &= help "Install libraries in the local project."
+      &= details [ "  Examples:"
+                 , "    elm-get install            # install everything needed by build.json"
+                 , "    elm-get install Vector2D   # install library from the central repo"
+                 , "    elm-get install sam/Array  # install a specific github repo" ]
     , Update { libs_ = [] &= args &= typ "LIBRARY" }
       &= help "Check for updates to any local libraries, ask to upgrade."
-    , Publish { repo = Nothing &= argPos 0 &= typ "REPO" }
+      &= details [ "  Examples:"
+                 , "    elm-get update             # check for updates to local libraries"
+                 , "    elm-get update Vector2D    # update library from the central repo"
+                 , "    elm-get update sam/Array   # update a specific github repo" ]
+    , Publish { repo = Nothing &= args &= typ "REPO" }
       &= help "Publish project to the central repository."
+      &= details []
     ]
 
 myModes :: Mode (CmdArgs Commands)
 myModes = cmdArgsMode $ modes commands
-    &= versionArg [explicit, name "version", name "v", summary _PROGRAM_INFO]
-    &= summary (_PROGRAM_INFO ++ ", (c) Evan Czaplicki 2013")
+    &= versionArg [explicit, name "version", name "v", summary info]
+    &= summary (info ++ ", (c) Evan Czaplicki 2013")
     &= help "install, update, and publish elm libraries"
     &= helpArg [explicit, name "help", name "h"]
     &= program "elm-get"
  
-_PROGRAM_INFO = "elm-get version " ++ showVersion This.version
+info = "elm-get " ++ showVersion This.version
 
 main :: IO ()
 main = do
-    args <- getArgs
-    -- If the user did not specify any arguments, pretend as "--help" was given
-    opts <- (if null args then withArgs ["--help"] else id) $ cmdArgsRun myModes
-    print opts
+  args <- getArgs
+  -- If the user did not specify any arguments, pretend as "--help" was given
+  opts <- (if null args then withArgs ["--help"] else id) $ cmdArgsRun myModes
+  print opts
