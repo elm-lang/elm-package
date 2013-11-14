58fb38948ad327a321cbb625f8e71b7e5bfe2a44
Evan Czaplicki <info@elm-lang.org>
2013-11-14 19:20:56 +0100

    Begin a Decode/ directory for extracting information from library info that is in JSON format

---
+++ Decode/Type.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Decode.Type where
+
+import Control.Applicative
+import Control.Monad
+import Data.Aeson
+import qualified Data.Text as Text
+import qualified Data.Vector as Vector
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text as Text
+import Data.Traversable (traverse)
+
+data Type = App Type Type
+          | Var Text.Text
+          | Record Type (HM.HashMap Text.Text Type)
+          | EmptyRecord
+            deriving (Show,Eq)
+
+instance FromJSON Type where
+  parseJSON value =
+    case value of
+      Object pairs ->
+          if HM.null pairs then return EmptyRecord else
+          do rawFields <- traverse parseJSON pairs
+             let ext = HM.lookupDefault EmptyRecord "_" rawFields
+             return $ Record ext (HM.delete "_" rawFields)
+                 
+      Array values ->
+          case Vector.length values of
+            0 -> mzero
+            1 -> parseJSON (Vector.head values)
+            _ -> do types <- traverse parseJSON values
+                    return $ Vector.foldl1 App types
+
+      String s -> return $ Var s
+
+      _ -> mzero
