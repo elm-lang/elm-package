bdae8f62b0f00baa2048842b8eb7721d8bcf2bf0
Evan Czaplicki <info@elm-lang.org>
2013-11-21 16:35:07 -0800

    Permit post requests and different data formats

--- Get/Registry.hs
+++ Get/Registry.hs
@@ -5,37 +5,44 @@ import Control.Monad.Error
 import Network.HTTP
 import Network.Stream
 import Network.URI (parseURI)
+import Model.Dependencies
+import Model.Name
 import Model.Version
+import Data.Version (showVersion)
+import qualified Paths_elm_get as This
+import qualified Data.Aeson as Json
 import qualified Data.Binary as Binary
 
 domain = "http://localhost:8000/"
 
-latest :: String -> ErrorT String IO String
+latest :: Name -> ErrorT String IO (Maybe Deps)
 latest library =
-    request (getRequest $ domain ++ "latest?library=" ++ library) id
+    request (domain ++ "latest?library=" ++ show library) $ \body ->
+        either throwError return $ Json.eitherDecode body
 
 versions :: String -> ErrorT String IO (Maybe [Version])
 versions library =
-    let parse = parseURI $ domain ++ "versions?library=" ++ library in
-    case parse of
-      Nothing -> throwError $ "could not construct request for versions of library " ++ library
-      Just uri -> request (mkRequest GET uri) Binary.decode
+    request (domain ++ "versions?library=" ++ library) (return . Binary.decode)
 
+{--
 register :: String -> String -> String -> ErrorT String IO String
 register library version docs =
     request (postRequestWithBody url "text/json" docs) id
   where
     url = domain ++ "register?library=" ++ library ++ "&version=" ++ version
+--}
 
-request :: HStream ty => Request ty -> (ty -> a) -> ErrorT String IO a
-request request handler = do
-  result <- liftIO $ simpleHTTP request `catch` \err ->
-                          let _ = err :: IOException
-                          in  return $ failMisc $ "Unable to connect to " ++ domain
-  case result of
-    Right response
-        | rspCode response == (2,0,0) -> return $ handler $ rspBody response
-        | otherwise -> throwError $ "request to registry was unsuccessful"
-
-    Left (ErrorMisc str) -> throwError str
-    Left err -> throwError (show err)
+request :: HStream ty => String -> (ty -> ErrorT String IO a) -> ErrorT String IO a
+request request handler =
+    case parseURI $ request ++ "&version=" ++ showVersion This.version of
+      Nothing  -> throwError $ "could not construct request: " ++ request
+      Just uri -> do 
+        result <- liftIO $ simpleHTTP (mkRequest GET uri) `catch`
+                      \err -> let _ = err :: IOException
+                              in return $ failMisc $ "Unable to connect to " ++ domain
+        case result of
+          Left (ErrorMisc str) -> throwError str
+          Left err -> throwError (show err)
+          Right response
+              | rspCode response == (2,0,0) -> handler $ rspBody response
+              | otherwise -> throwError $ "request to registry was unsuccessful"
