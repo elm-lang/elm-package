807f56076c8355d8ae23a75cd671337923d9a071
Evan Czaplicki <info@elm-lang.org>
2013-12-03 15:55:57 -0800

    Create listing.bits if it does not exist already

--- Registry/Generate/Listing.hs
+++ Registry/Generate/Listing.hs
@@ -2,17 +2,19 @@
 module Registry.Generate.Listing where
 
 import Control.Applicative
+import Control.Monad (when)
 import Data.Aeson as Json
 import Data.Binary as Binary
 import qualified Data.Map as Map
 import qualified Data.ByteString.Lazy as LBS
+import qualified System.Directory as Dir
 
 import qualified Registry.Utils as Utils
 import qualified Model.Dependencies as D
 
 add :: D.Deps -> IO ()
 add deps =
-    do bits <- LBS.readFile Utils.listingBits
+    do bits <- readBits
        let listings = Binary.decode bits
            name = show $ D.name deps
            version = show $ D.version deps
@@ -24,6 +26,14 @@ add deps =
            listings' = Map.alter insert name listings
        LBS.length bits `seq` LBS.writeFile Utils.listingBits (Binary.encode listings')
        LBS.writeFile Utils.listing $ Json.encode $ Map.elems listings'
+
+readBits :: IO LBS.ByteString
+readBits =
+    do exists <- Dir.doesFileExist Utils.listingBits
+       when (not exists) $
+            let empty = Binary.encode (Map.empty :: Map.Map String [String])
+            in  LBS.writeFile Utils.listingBits empty
+       LBS.readFile Utils.listingBits
 
 data Listing = Listing
     { name :: String
