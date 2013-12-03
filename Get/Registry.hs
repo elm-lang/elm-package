34ed47f7b2db13dfffc72584dc3a35244774b29b
Evan Czaplicki <info@elm-lang.org>
2013-12-03 12:42:15 -0800

    Clean up code and improve error when library hub cannot be reached

--- Get/Registry.hs
+++ Get/Registry.hs
@@ -3,12 +3,14 @@ module Get.Registry where
 
 import Network
 import Network.HTTP
+import Network.HTTP.Types
 import Network.HTTP.Conduit
 import Network.HTTP.Conduit.MultipartFormData
 
 import Control.Monad
 import Control.Monad.Error
 import Control.Monad.Trans.Resource
+import qualified Control.Exception as E
 
 import qualified Data.Aeson as Json
 import qualified Data.Binary as Binary
@@ -21,30 +23,30 @@ import qualified Model.Name         as N
 import qualified Model.Version      as V
 import qualified Get.Utils          as Utils
 
-import qualified Control.Exception as E
-import Network.HTTP.Types
-{--
 import Data.Version (showVersion)
 import qualified Paths_elm_get as This
---}
 
-domain = "http://localhost:8000/"
+domain = "http://localhost:8000"
+url path vars =
+    domain ++ "/" ++ path ++ "?" ++ urlEncodeVars (version : vars)
+  where
+    version = ("elm-get-version", showVersion This.version)
 
 metadata :: N.Name -> Manager -> ResourceT IO (Maybe D.Deps)
 metadata name manager =
-    do request  <- parseUrl $ domain ++ "metadata?" ++ urlEncodeVars [("library", show name)]
+    do request  <- parseUrl $ url "metadata" [("library", show name)]
        response <- httpLbs request manager
        return $ Json.decode $ responseBody response
 
 versions :: N.Name -> Manager -> ResourceT IO (Maybe [V.Version])
 versions name manager =
-    do request  <- parseUrl $ domain ++ "versions?" ++ urlEncodeVars [("library", show name)]
+    do request  <- parseUrl $ url "versions" [("library", show name)]
        response <- httpLbs request manager
        return $ Binary.decode $ responseBody response
 
 register :: N.Name -> V.Version -> FilePath -> Manager -> ResourceT IO ()
 register name version path manager =
-    do request <- parseUrl $ domain ++ "register?" ++ urlEncodeVars vars
+    do request <- parseUrl $ url "register" vars
        request' <- formDataBody files request
        httpLbs request' manager
        return ()
@@ -61,9 +63,14 @@ send request =
     where
       mkRequest = withSocketsDo $ withManager request
 
-      handler sce@(StatusCodeException (Status code err) headers _) =
-            return . Left $ BSC.unpack details
-          where
-            details = case List.lookup "X-Response-Body-Start" headers of
-                        Just msg | not (BSC.null msg) -> msg
-                        _ -> err
+      handler exception =
+          case exception of
+            sce@(StatusCodeException (Status code err) headers _) ->
+                let details = case List.lookup "X-Response-Body-Start" headers of
+                                Just msg | not (BSC.null msg) -> msg
+                                _ -> err
+                in  return . Left $ BSC.unpack details
+
+            _ -> return . Left $
+                 "probably unable to connect to <" ++ domain ++ "> (" ++
+                 show exception ++ ")"
