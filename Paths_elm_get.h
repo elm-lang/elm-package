69a3a043e88cda8cd94fc90985d08e2c2f9aa1f1
Evan Czaplicki <info@elm-lang.org>
2013-12-04 15:06:32 -0800

    Create file to make it easier to spoof having Paths_elm_get when using ghci for development

---
+++ Paths_elm_get.h
@@ -0,0 +1,10 @@
+module Paths_elm_get where
+
+import Data.Version
+import System.FilePath
+
+--getDataFileName :: FilePath -> IO FilePath
+--getDataFileName path = return ("dist" </> "data" </> path)
+
+version :: Version
+version = Version [0,1] []
