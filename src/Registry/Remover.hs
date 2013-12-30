622bea51ad11eba97604cfc996e3de952c426259
Evan Czaplicki <info@elm-lang.org>
2013-12-30 13:43:44 -0800

    Get removal closer to actually working

--- src/Registry/Remover.hs
+++ src/Registry/Remover.hs
@@ -1,13 +1,19 @@
-module Registry.Remover where
+module Main where
 
+import System.Directory
 import System.Environment
 import System.Exit
 import System.IO
 import Registry.Generate.Listing
 
+-- Run this program with:
+--   runhaskell -isrc src/Registry/Remover.hs [PROJECT]
+
 main = do
   args <- getArgs
   case args of
-    [name] -> remove name >> putStrLn "remove function called successfully"
-    _ -> do hPutStrLn stderr "Bad command line arguments. Expecting project name."
-            exitFailure
+    [name] -> do setCurrentDirectory "website"
+                 remove name
+                 putStrLn "remove function called successfully"
+    _      -> do hPutStrLn stderr "Bad command line arguments. Expecting a single project name."
+                 exitFailure
