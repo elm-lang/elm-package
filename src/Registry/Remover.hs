e5464e7f87ad9beb5a20822cf4896cf3f37d7cb1
Evan Czaplicki <info@elm-lang.org>
2013-12-30 13:56:48 -0800

    Get remover closer to working order

--- src/Registry/Remover.hs
+++ src/Registry/Remover.hs
@@ -1,19 +1,14 @@
 module Main where
 
-import System.Directory
 import System.Environment
 import System.Exit
 import System.IO
 import Registry.Generate.Listing
 
--- Run this program with:
---   runhaskell -isrc src/Registry/Remover.hs [PROJECT]
-
 main = do
   args <- getArgs
   case args of
-    [name] -> do setCurrentDirectory "website"
-                 remove name
+    [name] -> do remove name
                  putStrLn "remove function called successfully"
     _      -> do hPutStrLn stderr "Bad command line arguments. Expecting a single project name."
                  exitFailure
