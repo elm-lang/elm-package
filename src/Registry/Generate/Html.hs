a0e494fde1813031c52cf63fb346da69dd416f7a
Evan Czaplicki <info@elm-lang.org>
2014-01-25 18:19:53 +0100

    Make the links match the accent color

--- src/Registry/Generate/Html.hs
+++ src/Registry/Generate/Html.hs
@@ -38,8 +38,8 @@ adjustHtmlFile file =
 style :: BSC.ByteString
 style = 
     "<style type=\"text/css\">\n\
-    \  a:link {text-decoration: none; color: rgb(15,102,230);}\n\
-    \  a:visited {text-decoration: none; color: rgb(15,102,230);}\n\
+    \  a:link {text-decoration: none; color: rgb(96,181,204);}\n\
+    \  a:visited {text-decoration: none; color: rgb(96,181,204);}\n\
     \  a:active {text-decoration: none}\n\
     \  a:hover {text-decoration: underline; color: rgb(234,21,122);}\n\
     \  body { font-family: \"Lucida Grande\",\"Trebuchet MS\",\"Bitstream Vera Sans\",Verdana,Helvetica,sans-serif !important; }\n\
