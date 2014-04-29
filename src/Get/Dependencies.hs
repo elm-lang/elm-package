1c406055cb87c333be0867b937f4a7aaf0134986
Evan Czaplicki <info@elm-lang.org>
2014-04-29 19:55:26 +0200

    Cosmetic, mess with record style

--- src/Get/Dependencies.hs
+++ src/Get/Dependencies.hs
@@ -5,14 +5,14 @@ import qualified Elm.Internal.Name as N
 import qualified Elm.Internal.Version as V
 
 defaultDeps :: Deps
-defaultDeps = Deps {
-  name = N.Name "USER" "PROJECT",
-  version = V.V [0,1] "",
-  summary = "concise, helpful summary of your project",
-  description = "full description of this project, describe your use case",
-  license = "BSD3",
-  repo = "https://github.com/USER/PROJECT.git",
-  exposed = [],
-  elmVersion = V.elmVersion,
-  dependencies = []
-  }
+defaultDeps = Deps
+    { name = N.Name "USER" "PROJECT"
+    , version = V.V [0,1] ""
+    , summary = "concise, helpful summary of your project"
+    , description = "full description of this project, describe your use case"
+    , license = "BSD3"
+    , repo = "https://github.com/USER/PROJECT.git"
+    , exposed = []
+    , elmVersion = V.elmVersion
+    , dependencies = []
+    }
