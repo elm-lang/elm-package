6f35db81bda5a870ae465e15d927f8348c17b6a8
Andrew Shulaev <a-shulayev@ya.ru>
2014-07-08 11:00:21 +0200

    Added forcing a 80-characters limit on project summary

--- src/Get/Init.hs
+++ src/Get/Init.hs
@@ -9,25 +9,40 @@ import qualified Elm.Internal.Version as V
 import qualified Elm.Internal.Dependencies as D
 import qualified Data.ByteString.Lazy as BS
 
-askForChecked :: (String -> Maybe a) -> String -> String -> IO a
-askForChecked check request errorMsg = do
+askForChecked :: (String -> Either String a) -> String -> IO a
+askForChecked check request = do
   putStr $ request ++ " "
   hFlush stdout
   answer <- getLine
   case check answer of
-    Just result -> return result
-    Nothing -> do putStrLn errorMsg
-                  askForChecked check request errorMsg
+    Right result -> return result
+    Left message -> do putStrLn message
+                       askForChecked check request
 
-askForVersion req = askForChecked V.fromString req "Wrong version format!" 
-askFor req = askForChecked Just req undefined
+eitherFromMaybe :: a -> Maybe b -> Either a b
+eitherFromMaybe def val = case val of
+  Just r -> Right r
+  Nothing -> Left def
+
+askForVersion :: String -> IO V.Version
+askForVersion req = askForChecked check req
+  where check = (eitherFromMaybe "Wrong version format!" . V.fromString)
+
+askFor :: String -> IO String
+askFor req = askForChecked Right req
+
+askForLimited :: String -> Int -> String -> IO String
+askForLimited name limit req = askForChecked check req
+  where check str = if length str > limit
+                    then Left (concat [name, " length shouldn't exceed ", show limit, " characters!"])
+                    else Right str
 
 readDeps :: IO D.Deps
 readDeps = do
   projectName <- askFor "Project name:"
   userName <- askFor "Github user name:"
   version <- askForVersion "Initial version?"
-  summary <- askFor "Summary:"
+  summary <- askForLimited "Summary" 80 "Summary:"
   description <- askFor "Description:"
   license <- askFor "License?"
   repo <- askFor "Repository address?"
