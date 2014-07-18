e72f7eea7c63a178ebccbe6f450e931b4cd920af
Andrew Shulaev <a-shulayev@ya.ru>
2014-07-18 10:54:28 +0200

    Cleaned Get.Install code a little, added some comments

--- src/Get/Install.hs
+++ src/Get/Install.hs
@@ -20,17 +20,14 @@ import qualified Utils.Commands as Cmd
 import qualified Utils.Paths as Path
 import Utils.ResolveDeps
 
--- | Builds up the final transformation on the dependency file using
---   WriterT
-type InstallM = ErrorT String IO
-
 -- | External Interface
 installAll :: ErrorT String IO ()
 installAll =
   do deps <- D.depsAt EPath.dependencyFile
      libs <- solveConstraints deps
      currVersions <- L.getVersionsSafe EPath.librariesFile
-     let (diffList, libsToInstall) = computeDiff currVersions libs
+     let diffList = computeDiff currVersions libs
+     let (_, libsToInstall) = unzip diffList
      confirmed <- liftIO $ offerInstallPlan diffList
      case confirmed of
        False -> liftIO $ putStrLn "Bye."
@@ -40,63 +37,65 @@ installAll =
               writeLibraries libs
               putStrLn "Success!"
 
+-- | Just a synonym to tuple type which is used a lot in this module
 type Lib = (N.Name, V.Version)
-data LibChange = LibFresh N.Name V.Version
-               | LibUpdate N.Name V.Version V.Version -- name, old, new
 
-instance Show LibChange where
-  show change = case change of
-    LibFresh name version -> concat ["new: ", show name, " (", show version, ")"]
-    LibUpdate name old new -> concat ["upd: ", show name, " (", show old, " -> ", show new, ")"]
+-- | Datatype describing whether a particular library will be installed first time,
+--   or is updated from previous version
+data LibChange = LibFresh
+               | LibUpdate V.Version
 
-libFresh = uncurry LibFresh
+-- | Human-readable representation of previous datatype. Used to show "install plan"
+showUpdate :: (LibChange, Lib) -> String
+showUpdate (change, (name, new)) = case change of
+  LibFresh -> concat ["new: ", show name, " (", show new, ")"]
+  LibUpdate old -> concat ["upd: ", show name, " (", show old, " -> ", show new, ")"]
 
-computeDiff :: Maybe [Lib] -> [Lib] -> ([LibChange], [Lib])
+-- | Compute differences between currently installed libraries and new constraint solver result
+computeDiff :: Maybe [Lib] -> [Lib] -> [(LibChange, Lib)]
 computeDiff old new =
   case old of
-    Nothing -> (map libFresh new, new)
+    Nothing -> map (\x -> (LibFresh, x)) new
     Just libs ->
       let libsMap = Map.fromList libs
           change lib@(name, version) = case Map.lookup name libsMap of
             Just oldVersion ->
               if oldVersion == version
               then Nothing
-              else Just $ (LibUpdate name oldVersion version, lib)
-            Nothing -> Just $ (libFresh lib, lib)
-      in unzip $ mapMaybe change new
+              else Just $ (LibUpdate oldVersion, lib)
+            Nothing -> Just $ (LibFresh, lib)
+      in mapMaybe change new
 
-offerInstallPlan :: [LibChange] -> IO Bool
+-- | Print "install plan" to a used, ask to proceed
+offerInstallPlan :: [(LibChange, Lib)] -> IO Bool
 offerInstallPlan ls = case ls of
   [] -> do putStrLn "Nothing to install"
            return False
   _ -> do putStrLn "The following libraries will be installed:"
-          mapM_ print ls
+          mapM_ (putStrLn . showUpdate) ls
           putStr "Proceed? (y/n) "
           Cmd.yesOrNo
 
 install :: Library -> ErrorT String IO ()
 install _ = throwError "TODO: implement me"
 
-data InstallFlag = Create
-                 | NoCreate
-                 | Unknown
-                 deriving (Show, Read, Eq, Ord)
-
 -- | Write installed libraries to elm_dependencies/elm_libraries.json, which is used by compiler
-writeLibraries :: [(N.Name, V.Version)] -> IO ()
+writeLibraries :: [Lib] -> IO ()
 writeLibraries pairs =
   do let fromPair (n, v) = L.Library n v
          libraries = map fromPair pairs
      createDirectoryIfMissing True EPath.dependencyDirectory
      BS.writeFile EPath.librariesFile (encodePretty $ L.Libraries libraries)
 
-install1 :: (N.Name, V.Version) -> InstallM ()
+install1 :: Lib -> ErrorT String IO ()
 install1 (name, version) =
   Cmd.inDir EPath.dependencyDirectory $
   do repo <- Cmd.inDir Path.internals $ getRepoPath name version
      liftIO $ createDirectoryIfMissing True repo
      Cmd.copyDir (Path.internals </> repo) (repo </> show version)
 
+-- | Fetch (or update) a repository with given name, check out requested version,
+--   return a folder with package contents
 getRepoPath :: N.Name -> V.Version -> ErrorT String IO FilePath
 getRepoPath name version =
   do let directory = N.toFilePath name
