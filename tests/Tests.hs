39b1f8e075e55eec0f47537b5ad34a28696ad881
Evan Czaplicki <info@elm-lang.org>
2014-08-18 15:12:12 +0200

    cosmetic: move things around to try to make file easier to navigate

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -31,7 +31,81 @@ db1 = Map.fromList [ base, transformers, mtl ]
                             ])
                 ])
 
--- Helper functions to write inline test data more succintly
+test1 = fromError $ solveFake db1 (n "mtl") (v "1.0")
+test2 = fromError $ solveFake db1 (n "mtl") (v "2.0")
+test2sol = [(n "base", v "1.0"), (n "transformers", v "1.0"), (n "mtl", v "2.0")]
+
+main :: IO ()
+main =
+  do test1 >>= print
+     test2 >>= print
+
+fromError :: ErrorT String IO a -> IO a
+fromError action =
+  either fail return =<< runErrorT action
+
+
+-- CHECK FOR VALIDITY OF SOLUTIONS
+
+{-| Check whether given solution is really a solution. Supposed to use as
+a sanity check for existing tests and solver solutions
+-}
+isValidSolution :: FakeDB -> [(N.Name, V.Version)] -> Bool
+isValidSolution db solution =
+    all isConsistent solution
+  where
+    isConsistent (name, version) =
+      maybe False id $ do
+        versions <- Map.lookup name db of
+        constraints <- lookup version versions
+        return (all isSatisfied constraints)
+
+    isSatisfied (name, constraint) =
+      maybe False id $ do
+        version <- lookup name solution
+        return (C.satisfyConstraint constraint version)
+
+
+-- RUN SOLVER
+
+-- | Run dependency solver using stub data
+solveFake :: FakeDB -> N.Name -> V.Version -> ErrorT String IO [(N.Name, V.Version)]
+solveFake db name version =
+  do constraints <- readConstraints db name version
+     let libraryDb = toLibraryDb db
+         unreader = runReaderT (solveConstraintsByDeps name version constraints) $
+                    SolverEnv libraryDb (readConstraints db)
+         initialState = SolverState Map.empty Map.empty
+     (solved, state) <- runStateT unreader initialState
+     case solved of
+       False -> throwError "Failed to satisfy all the constraints :-("
+       True -> return $ Map.toList $ ssPinnedVersions state
+
+-- | A function passed to solver which "reads" constraints by name and version
+readConstraints :: FakeDB -> N.Name -> V.Version -> ErrorT String IO Constraints
+readConstraints db name version =
+    maybe notFound return $ do
+      versions <- Map.lookup name db
+      lookup version versions
+  where
+    notFound =
+      throwError $ "Could not find " ++ N.toString name ++ " " ++ V.toString version
+
+{-| Extract from stub data list of libraries and their version in
+format solver expects
+-}
+toLibraryDb :: FakeDB -> LibraryDB
+toLibraryDb fakeDb =
+    Map.fromList (map toLibraryEntry (Map.toList fakeDb))
+  where
+    toLibraryEntry (name, details) =
+      ( N.toString name
+      , LibraryInfo (N.toString name) "" (map fst details)
+      )
+
+
+-- SETUP HELPERS
+-- make it easier to create a FakeDb
 
 v :: String -> V.Version
 v = unsafeUnpackJust "version" . V.fromString
@@ -49,73 +123,3 @@ unsafeUnpackJust msg result =
     Nothing -> error $ "error unpacking " ++ msg ++ " from string"
 
 
-
-{-| Extract from stub data list of libraries and their version in
-format solver expects
--}
-fakeLibraryDb :: FakeDB -> LibraryDB
-fakeLibraryDb fakeDb =
-    Map.fromList (map toLibraryEntry (Map.toList fakeDb))
-  where
-    toLibraryEntry (name, details) =
-      ( N.toString name
-      , LibraryInfo (N.toString name) "" (map fst details)
-      )
-
--- | A function passed to solver which "reads" constraints by name and version
-readConstraints :: FakeDB -> N.Name -> V.Version -> ErrorT String IO Constraints
-readConstraints db name version =
-    maybe notFound return $ do
-      versions <- Map.lookup name db
-      lookup version versions
-  where
-    notFound =
-      throwError $ "Could not find " ++ N.toString name ++ " " ++ V.toString version
-
--- | Run dependency solver using stub data
-solveFake :: FakeDB -> N.Name -> V.Version -> ErrorT String IO [(N.Name, V.Version)]
-solveFake db name version =
-  do constraints <- readConstraints db name version
-     let libraryDb = fakeLibraryDb db
-         unreader = runReaderT (solveConstraintsByDeps name version constraints) $
-                    SolverEnv libraryDb (readConstraints db)
-         initialState = SolverState Map.empty Map.empty
-     (solved, state) <- runStateT unreader initialState
-     case solved of
-       False -> throwError "Failed to satisfy all the constraints :-("
-       True -> return $ Map.toList $ ssPinnedVersions state
-
-{-| Check whether given solution is really a solution. Supposed to use as
-a sanity check for existing tests and solver solutions
--}
-satisfyConstraints :: FakeDB -> [(N.Name, V.Version)] -> Bool
-satisfyConstraints db solution =
-    all isConsistent solution
-  where
-    isConsistent (name, version) =
-      maybe False id $ do
-        versions <- Map.lookup name db of
-        constraints <- lookup version versions
-        return (all isSatisfied constraints)
-
-    isSatisfied (name, constraint) =
-      maybe False id $ do
-        version <- lookup name solution
-        return (C.satisfyConstraint constraint version)
-
--- | Unpack ErrorT in IO: throw transformer's error as user IO error
-fromError :: ErrorT String IO a -> IO a
-fromError action =
-  do result <- runErrorT action
-     case result of
-       Right value -> return value
-       Left err -> fail err
-
-test1 = fromError $ solveFake db1 (n "mtl") (v "1.0")
-test2 = fromError $ solveFake db1 (n "mtl") (v "2.0")
-test2sol = [(n "base", v "1.0"), (n "transformers", v "1.0"), (n "mtl", v "2.0")]
-
-main :: IO ()
-main =
-  do test1 >>= print
-     test2 >>= print
