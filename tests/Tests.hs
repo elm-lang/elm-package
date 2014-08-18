61146136f508d916fecc9d84eedda52bd0130205
Evan Czaplicki <info@elm-lang.org>
2014-08-18 14:52:06 +0200

    style: clean up PR #56
      * Give longer, more helpful names in some cases.
      * Avoid deeply nested case-expressions when do-notation can be used.
      * Consistent list-literal style.

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -10,16 +10,29 @@ import qualified Elm.Internal.Constraint as C
 import qualified Elm.Internal.Version as V
 import qualified Elm.Internal.Name as N
 
--- | Differs from Data.Maybe.fromJust by having a mandatory error message
-unsafeUnpackJust :: String -> Maybe a -> a
-unsafeUnpackJust msg result =
-  case result of
-    Just v -> v
-    Nothing -> error $ "error unpacking " ++ msg ++ " from string"
+
+-- SETUP
 
 type FakeDB = Map N.Name [(V.Version, Constraints)]
 
--- | Helper function to write inline test data more succintly
+db1 :: FakeDB
+db1 = Map.fromList [ base, transformers, mtl ]
+  where
+    base =
+      (n "base", [ (v "0.1", []), (v "0.2", []), (v "1.0", []) ])
+
+    transformers =
+      (n "transformers", [ (v "1.0", [(n "base", c ">=1.0 <2.0")]) ])
+    
+    mtl =
+      (n "mtl", [ (v "1.0", [ (n "base", c ">=0.2 <1.0") ])
+                , (v "2.0", [ (n "base", c ">=0.2 <2.0")
+                            , (n "transformers", c ">=1.0 <2.0")
+                            ])
+                ])
+
+-- Helper functions to write inline test data more succintly
+
 v :: String -> V.Version
 v = unsafeUnpackJust "version" . V.fromString
 
@@ -29,33 +42,35 @@ c = unsafeUnpackJust "constraint" . C.fromString
 n :: String -> N.Name
 n = unsafeUnpackJust "name" . N.fromString . ("a/" ++)
 
-db1 :: FakeDB
-db1 = Map.fromList
-  [ (n "base", [(v "0.1", []), (v "0.2", []), (v "1.0", [])])
-  , (n "transformers", [(v "1.0", [(n "base", c ">=1.0 <2.0")])])
-  , (n "mtl", [(v "1.0", [(n "base", c ">=0.2 <1.0")]),
-               (v "2.0", [(n "base", c ">=0.2 <2.0"),
-                          (n "transformers", c ">=1.0 <2.0")])])
-  ]
+unsafeUnpackJust :: String -> Maybe a -> a
+unsafeUnpackJust msg result =
+  case result of
+    Just v -> v
+    Nothing -> error $ "error unpacking " ++ msg ++ " from string"
+
+
 
 {-| Extract from stub data list of libraries and their version in
 format solver expects
 -}
 fakeLibraryDb :: FakeDB -> LibraryDB
-fakeLibraryDb = Map.fromList . map (uncurry f) . Map.toList
+fakeLibraryDb fakeDb =
+    Map.fromList (map toLibraryEntry (Map.toList fakeDb))
   where
-    f name ls = (N.toString name, LibraryInfo (N.toString name) "" (map fst ls))
+    toLibraryEntry (name, details) =
+      ( N.toString name
+      , LibraryInfo (N.toString name) "" (map fst details)
+      )
 
 -- | A function passed to solver which "reads" constraints by name and version
 readConstraints :: FakeDB -> N.Name -> V.Version -> ErrorT String IO Constraints
 readConstraints db name version =
-  let result =
-        do versions <- Map.lookup name db
-           lookup version versions
-  in
-  case result of
-    Just r -> return r
-    Nothing -> throwError $ "Haven't found " ++ N.toString name ++ " " ++ V.toString version
+    maybe notFound return $ do
+      versions <- Map.lookup name db
+      lookup version versions
+  where
+    notFound =
+      throwError $ "Could not find " ++ N.toString name ++ " " ++ V.toString version
 
 -- | Run dependency solver using stub data
 solveFake :: FakeDB -> N.Name -> V.Version -> ErrorT String IO [(N.Name, V.Version)]
@@ -74,19 +89,19 @@ solveFake db name version =
 a sanity check for existing tests and solver solutions
 -}
 satisfyConstraints :: FakeDB -> [(N.Name, V.Version)] -> Bool
-satisfyConstraints db solution = all f solution
-  where f (name, version) =
-          case Map.lookup name db of
-            Nothing -> False
-            Just ls ->
-              case lookup version ls of
-                Nothing -> False
-                Just constraints ->
-                  all g constraints
-        g (name, constraint) =
-          case lookup name solution of
-            Just version -> C.satisfyConstraint constraint version
-            Nothing -> False
+satisfyConstraints db solution =
+    all isConsistent solution
+  where
+    isConsistent (name, version) =
+      maybe False id $ do
+        versions <- Map.lookup name db of
+        constraints <- lookup version versions
+        return (all isSatisfied constraints)
+
+    isSatisfied (name, constraint) =
+      maybe False id $ do
+        version <- lookup name solution
+        return (C.satisfyConstraint constraint version)
 
 -- | Unpack ErrorT in IO: throw transformer's error as user IO error
 fromError :: ErrorT String IO a -> IO a
