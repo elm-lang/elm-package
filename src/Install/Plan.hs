d897d46177bb5d39c4945ce9be0887d0a87f50da
Evan Czaplicki <info@elm-lang.org>
2014-09-27 19:49:21 -0700

    Get rid of tabs in source code

--- src/Install/Plan.hs
+++ src/Install/Plan.hs
@@ -11,45 +11,45 @@ data Plan = Plan
     { installs :: Map.Map N.Name V.Version
     , upgrades :: Map.Map N.Name (V.Version, V.Version)
     , removals :: Map.Map N.Name V.Version
-	}
+    }
 
 
 upgradePlan :: Dep.Solution -> Dep.Solution -> Plan
 upgradePlan old new =
-	Plan
-	{ installs = Map.difference new old
-  	, upgrades = discardNoOps (Map.intersectionWith Upgrade old new)
-  	, removals = Map.difference old new
-	}
+    Plan
+    { installs = Map.difference new old
+    , upgrades = discardNoOps (Map.intersectionWith Upgrade old new)
+    , removals = Map.difference old new
+    }
   where
-  	discardNoOps updates =
-  		Map.mapMaybe isChanged updates
+    discardNoOps updates =
+      Map.mapMaybe isChanged updates
 
-  	isChanged upgrade@(oldVersion,newVersion) =
-  		if oldVersion == newVersion
-  			then Nothing
-  			else Just upgrade
+    isChanged upgrade@(oldVersion,newVersion) =
+      if oldVersion == newVersion
+        then Nothing
+        else Just upgrade
 
 
--- DISPLAY  	
+-- DISPLAY
 
 display :: Plan -> String
 display (Plan installs upgrades removals) =
-	displayCategory "Install" displayInstall installs
-	++ displayCategory "Upgrade" displayUpgrade upgrades
-	++ displayCategory "Remove" displayRemove removals
+  displayCategory "Install" displayInstall installs
+  ++ displayCategory "Upgrade" displayUpgrade upgrades
+  ++ displayCategory "Remove" displayRemove removals
   where
-  	displayCategory name render category =
-  		if Map.null category then "" else
-	  		"  " ++ name ++ ":"
-	  		++ concatMap (\entry -> "\n    " ++ render entry) (Map.toList category)
-	  		++ "\n"
+    displayCategory name render category =
+      if Map.null category then "" else
+        "  " ++ name ++ ":"
+        ++ concatMap (\entry -> "\n    " ++ render entry) (Map.toList category)
+        ++ "\n"
 
-	displayInstall (name, version) =
-  		N.toString name ++ " " ++ V.toString version
+  displayInstall (name, version) =
+      N.toString name ++ " " ++ V.toString version
 
-  	displayUpgrade (name, (old, new)) =
-		N.toString name ++ " (" ++ V.toString old ++ " -> " ++ V.toString new ++ ")"
+    displayUpgrade (name, (old, new)) =
+    N.toString name ++ " (" ++ V.toString old ++ " -> " ++ V.toString new ++ ")"
 
-	displayRemove (name, _version) =
-		N.toString name
+  displayRemove (name, _version) =
+    N.toString name
