13c86eeed33b14c695438182743c0ac008232857
Evan Czaplicki <info@elm-lang.org>
2014-09-28 19:59:21 -0700

    Changed solved-dependencies.json file name

--- src/Elm/Package/Paths.hs
+++ src/Elm/Package/Paths.hs
@@ -3,16 +3,20 @@ module Elm.Package.Paths where
 
 import System.FilePath ((</>))
 
+
 {-| Name of directory for all of a project's dependencies. -}
 packagesDirectory :: FilePath
-packagesDirectory = "elm_packages"
+packagesDirectory =
+	"elm_packages"
+
 
 {-| Describes the exact versions of every library used for your project. This
-information is written by elm-get when it solves and installs dependencies.
+information is written by elm-package when it solves and installs dependencies.
 -}
-dependencies :: FilePath
-dependencies =
-    packagesDirectory </> "exact-dependencies.json"
+solvedDependencies :: FilePath
+solvedDependencies =
+    packagesDirectory </> "solved-dependencies.json"
+
 
 {-| Name of the dependency file, specifying dependencies and other metadata
 for building and sharing projects.
