157e577719288cea0a40d67a64992fb5f6051ca1
Evan Czaplicki <info@elm-lang.org>
2014-09-30 13:35:58 -0700

    Add an Environment to the Manager

--- src/Manager.hs
+++ src/Manager.hs
@@ -1,16 +1,24 @@
 module Manager where
 
-import Control.Monad.Reader
 import Control.Monad.Error
+import Control.Monad.Reader
+import qualified System.Directory as Dir
+import System.FilePath ((</>))
 
 
-type Manager a =
-    ErrorT String IO a
+type Manager =
+    ErrorT String (ReaderT Environment IO)
 
 
 data Environment = Environment
     { catalog :: String
+    , cacheDirectory :: FilePath
     }
+
+
+run :: Environment -> Manager a -> IO (Either String a)
+run environment manager =
+    runReaderT (runErrorT manager) environment
 
 
 data Command
@@ -18,6 +26,9 @@ data Command
     | Publish
 
 
-run :: Manager a -> IO (Either String a)
-run manager =
-    runErrorT manager
+getCacheDirectory :: IO FilePath
+getCacheDirectory =
+    do  root <- Dir.getAppUserDataDirectory "elm"
+        let dir = root </> "package"
+        Dir.createDirectoryIfMissing True dir
+        return dir
