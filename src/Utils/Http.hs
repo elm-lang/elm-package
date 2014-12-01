ed61e6572aca9b3110e07136e1d9b146abeacd08
Evan Czaplicki <info@elm-lang.org>
2014-11-30 23:21:30 -0800

    Try to make Http code catch more errors

--- src/Utils/Http.hs
+++ src/Utils/Http.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Utils.Http where
+module Utils.Http (send) where
 
 import qualified Control.Exception as E
 import Control.Monad.Error
@@ -14,23 +14,45 @@ import Network.HTTP.Types
 
 send
     :: (MonadIO m, MonadError String m)
-    => String -> (Request -> Manager -> IO a) -> m a
+    => String
+    -> (Request -> Manager -> IO a)
+    -> m a
 send url handler =
-    do result <- liftIO $ E.catch (Right `fmap` sendRequest) handleError
-       either throwError return result
-    where
-      sendRequest = do
-        request <- parseUrl url
-        withSocketsDo $ withManager tlsManagerSettings (handler request)
+  do  result <- liftIO (sendSafe url handler)
+      either throwError return result
 
-      handleError exception =
-          case exception of
-            StatusCodeException (Status _code err) headers _ ->
-                let details = case List.lookup "X-Response-Body-Start" headers of
-                                Just msg | not (BSC.null msg) -> msg
-                                _ -> err
-                in  return . Left $ BSC.unpack details
 
-            _ -> return . Left $
-                 "failed with '" ++ show exception ++ "' when sending request to\n" ++
-                 "    <" ++ url ++ ">"
+sendSafe :: String -> (Request -> Manager -> IO a) -> IO (Either String a)
+sendSafe url handler =
+    sendUnsafe url handler
+      `E.catch` handleHttpError url
+      `E.catch` (handleAnyError url :: E.SomeException -> IO (Either String b))
+
+
+
+sendUnsafe :: String -> (Request -> Manager -> IO a) -> IO (Either err a)
+sendUnsafe url handler =
+  do  request <- parseUrl url
+      result <- withSocketsDo $ withManager tlsManagerSettings (handler request)
+      return (Right result)
+
+
+handleHttpError :: String -> HttpException -> IO (Either String b)
+handleHttpError url exception =
+  case exception of
+    StatusCodeException (Status _code err) headers _ ->
+        let details =
+              case List.lookup "X-Response-Body-Start" headers of
+                Just msg | not (BSC.null msg) -> msg
+                _ -> err
+        in
+            return . Left $ BSC.unpack details
+
+    _ -> handleAnyError url exception
+
+
+handleAnyError :: (E.Exception e) => String -> e -> IO (Either String b)
+handleAnyError url exception =
+  return . Left $
+      "failed with '" ++ show exception ++ "' when sending request to\n" ++
+      "    <" ++ url ++ ">"
