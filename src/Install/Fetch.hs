18f0888013d80c5c285d9af453f68fbc7f9cd50e
Evan Czaplicki <info@elm-lang.org>
2014-12-02 22:47:25 -0800

    Correct bug that skipped upgrades of existing packages

--- src/Install/Fetch.hs
+++ src/Install/Fetch.hs
@@ -6,6 +6,7 @@ import qualified Codec.Archive.Zip as Zip
 import qualified Data.List as List
 import qualified Network.HTTP.Client as Client
 import System.Directory (doesDirectoryExist, getDirectoryContents, renameDirectory)
+import System.FilePath ((</>))
 
 import qualified Elm.Package.Name as N
 import qualified Elm.Package.Version as V
@@ -15,7 +16,7 @@ import qualified Utils.Http as Http
 
 package :: (MonadIO m, MonadError String m) => N.Name -> V.Version -> m ()
 package name@(N.Name user _) version =
-  ifNotExists directory $ do
+  ifNotExists name version $ do
       Http.send zipball extract
       files <- liftIO $ getDirectoryContents "."
       case List.find (List.isPrefixOf user) files of
@@ -24,17 +25,17 @@ package name@(N.Name user _) version =
         Just dir ->
             liftIO $ renameDirectory dir (V.toString version)
   where
-    directory = N.toFilePath name
     zipball =
         "http://github.com/" ++ N.toUrl name ++ "/zipball/" ++ V.toString version ++ "/"
 
 
-ifNotExists :: (MonadIO m, MonadError String m) => FilePath -> m () -> m ()
-ifNotExists directory command =
-    do exists <- liftIO $ doesDirectoryExist directory
-       if exists
-          then return ()
-          else Cmd.inDir directory command
+ifNotExists :: (MonadIO m, MonadError String m) => N.Name -> V.Version -> m () -> m ()
+ifNotExists name version command =
+  do  let directory = N.toFilePath name
+      exists <- liftIO $ doesDirectoryExist (directory </> V.toString version)
+      if exists
+        then return ()
+        else Cmd.inDir directory command
 
 
 extract :: Client.Request -> Client.Manager -> IO ()
