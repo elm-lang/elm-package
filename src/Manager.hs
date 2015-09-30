7a614594183471530bf415b03b0f57d6f959f322
Evan Czaplicki <info@elm-lang.org>
2015-09-30 15:51:38 -0700

    Point to dev version of package website for now

--- src/Manager.hs
+++ src/Manager.hs
@@ -24,7 +24,7 @@ data Environment = Environment
 defaultEnvironment :: IO Environment
 defaultEnvironment =
     do  cacheDirectory <- getCacheDirectory
-        return (Environment "http://package.elm-lang.org" cacheDirectory)
+        return (Environment "http://package.elm-lang.org:1234" cacheDirectory)
 
 
 getCacheDirectory :: IO FilePath
