fc0924210fe5a7c0af543769b1353dbb2ddf2f0c
Evan Czaplicki <info@elm-lang.org>
2016-05-07 19:46:06 -0700

    Point at the real package website

--- src/Manager.hs
+++ src/Manager.hs
@@ -24,7 +24,7 @@ data Environment = Environment
 defaultEnvironment :: IO Environment
 defaultEnvironment =
     do  cacheDirectory <- getCacheDirectory
-        return (Environment "http://159.203.88.24:8000" cacheDirectory)
+        return (Environment "http://package.elm-lang.org" cacheDirectory)
 
 
 getCacheDirectory :: IO FilePath
