718f55727e0a582e585897b5fdfabef2e3897bd5
Evan Czaplicki <info@elm-lang.org>
2016-05-29 01:53:56 -0700

    Switch Package.* to Pkg.*

--- src/Install/Fetch.hs
+++ src/Install/Fetch.hs
@@ -7,16 +7,15 @@ import qualified Network.HTTP.Client as Client
 import System.Directory (doesDirectoryExist, getDirectoryContents, renameDirectory)
 import System.FilePath ((</>))
 
-import qualified Elm.Package as Package
+import qualified Elm.Package as Pkg
 import qualified CommandLine.Helpers as Cmd
 import qualified Manager
 import qualified Reporting.Error as Error
 import qualified Utils.Http as Http
 
 
-
-package :: Package.Name -> Package.Version -> Manager.Manager ()
-package name@(Package.Name user _) version =
+package :: Pkg.Name -> Pkg.Version -> Manager.Manager ()
+package name@(Pkg.Name user _) version =
   ifNotExists name version $
     do  Http.send (toZipballUrl name version) extract
         files <- liftIO $ getDirectoryContents "."
@@ -25,20 +24,20 @@ package name@(Package.Name user _) version =
             throwError $ Error.ZipDownloadFailed name version
 
           Just dir ->
-            liftIO $ renameDirectory dir (Package.versionToString version)
+            liftIO $ renameDirectory dir (Pkg.versionToString version)
 
 
-toZipballUrl :: Package.Name -> Package.Version -> String
+toZipballUrl :: Pkg.Name -> Pkg.Version -> String
 toZipballUrl name version =
-  "https://github.com/" ++ Package.toUrl name
-  ++ "/zipball/" ++ Package.versionToString version ++ "/"
+  "https://github.com/" ++ Pkg.toUrl name
+  ++ "/zipball/" ++ Pkg.versionToString version ++ "/"
 
 
 
-ifNotExists :: Package.Name -> Package.Version -> Manager.Manager () -> Manager.Manager ()
+ifNotExists :: Pkg.Name -> Pkg.Version -> Manager.Manager () -> Manager.Manager ()
 ifNotExists name version command =
-  do  let directory = Package.toFilePath name
-      exists <- liftIO $ doesDirectoryExist (directory </> Package.versionToString version)
+  do  let directory = Pkg.toFilePath name
+      exists <- liftIO $ doesDirectoryExist (directory </> Pkg.versionToString version)
       if exists
         then return ()
         else Cmd.inDir directory command
